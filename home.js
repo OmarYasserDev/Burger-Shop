3415da8ed3e87b8c9fb6f50b185ca3b7e5af83c7
OmarYasserDev <omar1221223@gmail.com>
2026-06-19 21:37:50 +0300

    most of the html and the css finshed, I added the products, the css the js to change between them

--- home.js
+++ home.js
@@ -13,3 +13,26 @@ const searchBox = document.getElementById("searchBox");
 searchBtn.addEventListener("click", () => {
     searchBox.classList.toggle("show");
 });
+const items = document.querySelectorAll(".item");
+
+const overlay = document.querySelector(".overlay");
+const modalImg = document.querySelector(".modal-img");
+const modalTitle = document.querySelector(".modal-title");
+const modalPrice = document.querySelector(".modal-price");
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        const img = item.querySelector(".img-pro").src;
+        const title = item.querySelector("h4").textContent;
+        const price = item.querySelector("p").textContent;
+        modalImg.src = img;
+        modalTitle.textContent = title;
+        modalPrice.textContent = price;
+        overlay.classList.add("show");
+    });
+});
+overlay.addEventListener("click", (e) => {
+    if(e.target === overlay){
+        overlay.classList.remove("show");
+    }
+});
